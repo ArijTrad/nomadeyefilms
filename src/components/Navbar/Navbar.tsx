d385a6e8a1a38f2563ca930ab66a2fdfac9d9080
ArijTrad <astra.arij@gmail.com>
2026-06-19 12:29:47 +0300

    save changes just in case

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,28 @@
 import styles from './Navbar.module.css'
-
 import UiLink from '../ui/UiLink/UiLink'
-//import Button from '../ui/Button/Button'
+import UiButton from '../ui/UiButton/UiButton'
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      
-      {/*<component-ui-element>xxxxx</component-ui-element>*/}
-      <UiLink href="#hello" variant="nav">trial </UiLink>
-
       {/* LEFT */}
-      <div className={styles.left}>
+      <a href="/" className={styles.left}>
         <img className={styles.leftlogonavbar} src="/images/nomadeyefilmlogo.svg" alt="Nomad Eye Films" />
         <span className={styles.logo}>NOMAD EYE FILMS</span>
-      </div>
+      </a>
 
       {/* CENTER */}
       <div className={styles.center}>
-        <a href="#">Home</a>
-        <a href="#">Features</a>
-        <a href="#">Pricing</a>
-        <a href="#">About</a>
+        <UiLink href="/" variant="nav">Home</UiLink>
+        <UiLink href="#" variant="nav">Features</UiLink>
+        <UiLink href="#" variant="nav">Pricing</UiLink>
+        <UiLink href="#" variant="nav">About</UiLink>
       </div>
 
       {/* RIGHT */}
       <div className={styles.right}>
-        <button className={styles.btn}>Get Started</button>
+        <UiButton variant="primary">Get Started</UiButton>
       </div>
-
     </div>
   )
 }
