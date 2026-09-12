ec681e54cfccc6132954fc4058da18f754a61b1b
ArijTrad <astra.arij@gmail.com>
2026-09-12 21:59:58 +0300

    video modal updated, made one resusable component nad made hero use it

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,23 +2,18 @@
 
 //libraries, tools imports
 import { useState } from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
-
-
-
 
 //docs, assets, imports LOCAL
 import styles from './Hero.module.css'
 import UiButton from '../ui/UiButton/UiButton';
-
 import heroPreviewVideo from "../../assets/videos/herothumbnail/showreel_2025_240p.mp4"
+import VideoModal from '../ui/VideoModal/VideoModal';
 
 
 export default function Hero() {
 
         const [isVideoOpen, setIsVideoOpen] = useState(false);
         const VIMEO_VIDEO_ID = "1084636349";
-        const VIMEO_EMBED_URL = `https://player.vimeo.com/video/${VIMEO_VIDEO_ID}?autoplay=1`;
 
 
     return (
@@ -94,54 +89,12 @@ export default function Hero() {
             </section>
 
 
-
-
-
-
-
-
-
-
-
-<Dialog.Root
+<VideoModal
+    vimeoId={VIMEO_VIDEO_ID}
+    title="Nomad Eye Films Showreel"
     open={isVideoOpen}
     onOpenChange={setIsVideoOpen}
->
-    <Dialog.Portal>
-
-        <Dialog.Overlay className={styles.dialogOverlay} />
-
-        <Dialog.Content className={styles.dialogContent}>
-
-            <Dialog.Title className={styles.visuallyHidden}>
-                Showreel Video
-            </Dialog.Title>
-
-            <iframe 
-                className={styles.vimeoFrame}
-                src={VIMEO_EMBED_URL}
-                title="Nomad Eye Films Showreel"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowFullScreen
-            />
-
-            <Dialog.Close className={styles.dialogClose}>
-                ✕
-            </Dialog.Close>
-
-        </Dialog.Content>
-
-    </Dialog.Portal>
-</Dialog.Root>
-
-
-
-
-
-
-
-
-
+/>
 
 
 
